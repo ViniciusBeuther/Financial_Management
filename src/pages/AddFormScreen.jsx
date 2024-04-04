a6d219fe3d45d08f066d740b0e2ae7e653f7831f
ViniciusBeuther <vinicius.beuther15@gmail.com>
2024-04-04 19:18:15 -0300

    New: Clean form

--- src/pages/AddFormScreen.jsx
+++ src/pages/AddFormScreen.jsx
@@ -1,5 +1,5 @@
 import {
-    Button,
+  Button,
   Checkbox,
   InputLabel,
   MenuItem,
@@ -8,9 +8,9 @@ import {
   Select,
   Typography,
 } from "@mui/material";
-import Header from "../Components/Header";
 import { FaBrazilianRealSign } from "react-icons/fa6";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { supabase } from "../API/Initialization";
 
 const AddFormScreen = () => {
   const [categories, setCategories] = useState([
@@ -134,16 +134,17 @@ const AddFormScreen = () => {
   const sortedCategories = categories.slice().sort((a, b) => {
     return a.item.localeCompare(b.item);
   });
+  const [selectedValue, setSelectedValue] = useState(""); // Select checked checkbox
+  const [selectedInput, setSelectedInput] = useState(""); // Select the category value
 
-  const [selectedValue, setSelectedValue] = useState("");
-
-  const [formData, setFormData] = useState({
-    category: '',
-    amount: 0.00,
-    date: '',
-    payment_form: '',
-    description: ''
-  });
+  // Object to insert in the DB
+  const ObjToInsert = {
+    category: "",
+    amount: 0.0,
+    date: "",
+    payment_form: "",
+    description: "",
+  };
 
   const handleChange = (event) => {
     setSelectedValue(event.target.value);
@@ -151,35 +152,89 @@ const AddFormScreen = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const formInputs = Array.from(event.target.elements).filter(element =>
-      ['INPUT', 'SELECT', 'TEXTAREA'].includes(element.tagName)
-    );
-    formInputs.forEach((input) => console.log(input.value))
-  }
+
+    try {
+      ObjToInsert.category =
+        document.getElementById("form__category").textContent;
+    } catch (error) {
+      console.log("Um probleminha, pequeno bug...");
+    }
+
+    // Pick and set up values to insert in DB
+    ObjToInsert.amount = document.getElementById("addform__amount").value;
+    ObjToInsert.amount = (ObjToInsert.amount * 1).toFixed(2);
+    ObjToInsert.date = document.getElementById("addform__date").value;
+    ObjToInsert.payment_form = selectedValue;
+    ObjToInsert.description = document.getElementById("addform__textarea").value;
+    
+    async function insert(Obj){
+        try{                
+            const { data, error } = await supabase
+            .from('gerenciador_financeiro')
+            .insert([
+            { 
+                date: Obj.date,
+                amount: (Obj.amount * 1).toFixed(2),
+                category: Obj.category,
+                payment_method: Obj.payment_method,
+                description: Obj.description,
+                type: "expanse"
+            },
+            ])
+            .select()
+                    
+        } catch(error){
+            console.log(error.message);
+        }
+    }
+
+    insert(Object)
+
+    //console.log(ObjToInsert);
+
+    // Reset Select and Radio fields from
+    setSelectedValue(""); // Limpa o RadioGroup
+    setSelectedInput(""); // Limpa o Select
+
+    // Clean text inputs
+    document.getElementById("addform__textarea").value = "";
+    document.getElementById("addform__amount").value = "";
+    document.getElementById("addform__date").value = "";
+  };
 
   return (
-    <section className="bg-secundary">  
+    <section className="bg-secundary">
       <article className="w-full p-2">
         <Typography className="text-primary p-2 text-center" variant="h4">
           Lançamentos
         </Typography>
 
-        <form onSubmit={(ev) => handleSubmit(ev)} className="flex flex-col gap-2">
+        <form
+          onSubmit={(ev) => handleSubmit(ev)}
+          className="flex flex-col gap-2"
+        >
           <label className="text-primary text-xl">Categoria</label>
 
           <Select
             label="Categoria"
             className="bg-white text-primary w-full rounded-lg"
+            id="form__category"
+            value={selectedInput}
+            onChange={(ev) => setSelectedInput(ev.target.value)}
           >
             {/* Menu Items, using the sorted categories */}
             {sortedCategories.map((obj, index) => (
-              <MenuItem value={obj.item} id={`addform__menuitem-${index}`}>
+              <MenuItem
+                key={index}
+                value={obj.item}
+                id={`addform__menuitem-${index}`}
+              >
                 {obj.icon} {obj.item}
               </MenuItem>
             ))}
           </Select>
 
-            {/* Amount input */}
+          {/* Amount input */}
           <div className="flex flex-col">
             <label className="text-primary text-xl">Valor</label>
             <div className="flex items-center">
@@ -194,7 +249,7 @@ const AddFormScreen = () => {
             </div>
           </div>
 
-            {/* Date + Radio group inputs */}
+          {/* Date + Radio group inputs */}
           <div className="flex flex-col lg:flex-row lg:items-center lg:justify-around text-primary">
             <article>
               <label className="text-primary text-xl">Data</label>
@@ -202,7 +257,7 @@ const AddFormScreen = () => {
                 <input
                   type="date"
                   name="amount"
-                  id="addform__amount"
+                  id="addform__date"
                   placeholder="0.00"
                   className="p-4 rounded-lg w-full bg-white"
                 />
@@ -229,13 +284,25 @@ const AddFormScreen = () => {
             </article>
           </div>
 
-            {/* Description input */}
-            <div className="mb-2">
-                <label className="text-primary text-xl">Descrição (opcional)</label>
-                <input type="text" name="addform__textarea" id="addform__textarea" className="p-4 w-full rounded-md" />
-            </div>
+          {/* Description input */}
+          <div className="mb-2">
+            <label className="text-primary text-xl">Descrição (opcional)</label>
+            <input
+              type="text"
+              name="addform__textarea"
+              id="addform__textarea"
+              className="p-4 w-full rounded-md"
+            />
+          </div>
 
-            <Button variant="contained" size="large" color="success" type="submit">Adicionar</Button>
+          <Button
+            variant="contained"
+            size="large"
+            color="success"
+            type="submit"
+          >
+            Adicionar
+          </Button>
         </form>
       </article>
     </section>
