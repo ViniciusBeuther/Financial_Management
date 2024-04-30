4770b1a8abc619145f5ece2fd281b1d6385d066e
ViniciusBeuther <vinicius.beuther15@gmail.com>
2024-04-30 13:02:34 -0300

    Upd: Credit Card Box
    Adjusted the initial styling in home page, added a new look to the credit card

--- src/pages/AccountInfo.jsx
+++ src/pages/AccountInfo.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import TransactionList from "../Components/TransactionList";
 import ActionButtons from "../Components/ActionButtons";
 import moneyIcon from "../../assets/icons/money icon.svg"
+import creditCardIcon from "../../assets/icons/credit card icon.svg"
 
 const AccountInfo = (props) => {
   const [balance, setBalance] = useState(0.0);
@@ -25,30 +26,40 @@ const AccountInfo = (props) => {
 
   return (
     <>
-      <div className="text-center flex items-center justify-start gap-5 mt-5 bg-white m-2 p-2 rounded-lg">
-        <div className="bg-green-300 flex items-center justify-center p-3 rounded-lg">
-          <img src={moneyIcon} alt="money-icon" width={32} height={32} className="" />
-        </div>
-        <div className="flex items-start flex-col">
-          <Typography>Saldo:</Typography>
-          <Typography
-            variant="h5" 
-            className="accountInfo__container_balance text-primary tracking-tight"
-          >
-            ${balance.toLocaleString()}
-          </Typography>
-        </div>
-      </div>
+      <article className="flex items-center justify-between">
+          <div className="text-center flex items-center justify-start gap-2 w-full bg-white m-2 p-2 rounded-lg">
+            <div className="bg-green-300 flex items-center justify-center p-3 rounded-lg">
+              <img src={moneyIcon} alt="money-icon" width={32} height={32} className="" />
+            </div>
+            <div className="flex items-start flex-col">
+              <Typography><strong>Saldo:</strong></Typography>
+              <Typography
+                variant="h5" 
+                className="accountInfo__container_balance text-primary tracking-tight"
+              >
+                ${balance.toLocaleString()}
+              </Typography>
+            </div>
+          </div>
 
-      <div className="text-center flex items-start justify-center flex-col bg-white m-2 p-2 rounded-lg">
-        <Typography variant="h6">Fatura:</Typography>
-        <Typography
-          variant="h3"
-          className="accountInfo__container_balance text-primary tracking-tight"
-        >
-          ${balance.toLocaleString()}
-        </Typography>
-      </div>
+          <div className="text-center flex items-center justify-start w-full bg-white m-2 p-2 rounded-lg">
+            <div className="bg-yellow-300 flex items-center justify-center p-3 rounded-lg">
+              <img src={creditCardIcon} alt="credit-card-icon" width={32} height={32} />
+            </div>
+
+            <div className="flex items-start flex-col ml-2">
+              <Typography><strong>Fatura:</strong></Typography>
+              <Typography
+                variant="h5"
+                className="accountInfo__container_balance text-primary tracking-tight"
+              >
+                ${balance.toLocaleString()}
+              </Typography>
+            </div>
+          </div>
+
+      </article>
+
       
       {/* Action buttons */}
       <ActionButtons />
