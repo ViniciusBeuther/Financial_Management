4ed508e9c5af3f12d803b10571c0cf81a315ddcb
ViniciusBeuther <vinicius.beuther15@gmail.com>
2024-05-20 18:20:44 -0300

    New: Header
    Added a header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,19 @@
-import { Typography } from "@mui/material"
+import { Typography } from "@mui/material";
+import savingsIcon from "../../assets/icons/savings icon.svg";
 
 const Header = () => {
-    return(
-        <Typography variant="h5" className="titleGradient">
-            
-        </Typography>
-    )
-}
+  return (
+    <h4 className="titleGradient text-3xl pb-2 mx-5 mb-5 mt-2 border-b-2 flex items-center">
+      BFinance
+      <img
+        src={savingsIcon}
+        alt="savings-icon"
+        width={48}
+        height={48}
+        className="drop-shadow-sm"
+      />
+    </h4>
+  );
+};
 
-export default Header
+export default Header;
